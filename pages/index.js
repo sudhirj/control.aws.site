7ab0198fbae6a7a80e1ebe03e7f74ff231c66387
Sudhir Jonathan <sudhir.j@gmail.com>
2021-06-10 12:53:44 +0530

    Adding primitive definitions.

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
       <div className="px-4">
         <Heading>CONTROL for AWS</Heading>
         <SubHeading>
-          CONTROL is a suite of tools (CLI, GUI and notes) that help you setup,
+          CONTROL is a suite of tools — CLI, GUI and Docs — that help you setup,
           configure and manage your applications and data on AWS. They apply
           best practices for security, performance and cost-optimization by
           default, and help you override these defaults when you need to.
@@ -29,12 +29,57 @@ export default function Home() {
         <dl>
           <DT>Why?</DT>
           <DD>
-            AWS is a collection of services and tools that each do a specific
-            thing well. Using AWS effectively and securely requires
-            understanding each of these tools and knowing how to use them with
-            each other. This requires a lot of research, study and
-            experimentation. The authors have done quite a bit of that, and
-            written everything we know into CONTROL.
+            <p>
+              AWS is a collection of services and tools that each do a specific
+              thing well. Using AWS effectively and securely requires
+              understanding each of these tools and knowing how to use them with
+              each other. This requires a lot of research, study and
+              experimentation — but not everybody needs to learn everything.
+              CONTROL collects ideas and best practices and packages them into a
+              usable system.
+            </p>
+            <p>
+              It also takes a lot of work to create security groups and IAM
+              roles for each part of the application and link to them together
+              on a least-privilege basis, so automated tools to set up these
+              links correctly and analyze them makes it much easier to be secure
+              by default.
+            </p>
+          </DD>
+          <DT>The Basics</DT>
+          <DD>
+            Your apps are organized into <em>Services</em>, <em>Tasks</em>,{" "}
+            <em>Datastores</em>, and <em>Topics</em>.
+            <ul>
+              <li>
+                <strong>Services</strong> are always-running processes that
+                listen for and respond to to HTTP traffic. They are defined by
+                combination of a container and execution command, and they
+                receive a PORT to bind to. Examples are websites, APIs, gRPC
+                services, WebSocket services, etc. They can run on one or more
+                of ECS, EKS, Fargate, App Runner, Lambda, and be load balanced
+                by ALB, ELB, API Gateway.
+              </li>
+              <li>
+                <strong>Tasks</strong> are processes that may be always-running
+                or scheduled periodically, and they do their work without
+                listening to any incoming traffic. They are defined by a
+                combination of container and execution command. Examples are
+                daily / hourly batch jobs, queue / task workers, etc. They might
+                run on one or more of ECS, EKS, Fargate, or Lambda.
+              </li>
+              <li>
+                <strong>Datastores</strong> are instances or clusters on any of
+                the managed data services available, like RDS, ElastiCache, S3,
+                DynamoDB, DocumentDB, Neptune, Elasticsearch, and more.
+              </li>
+              <li>
+                <strong>Topics</strong> are messaging systems and queues between
+                different applications and services, which consist of SNS topics
+                for publishing messages to, and HTTP or SQS subscriptions for
+                processing these messages.
+              </li>
+            </ul>
           </DD>
         </dl>
       </div>
